99e9963646b5b9ca8de1e720287c7d7150337ae2
MarioCampelloRodes <mcampellor@esimurcia.com>
2024-03-11 12:33:17 +0100

    Level Selector Movement

--- Assets/Code/Scripts/LevelSelector/LSPlayer.cs
+++ Assets/Code/Scripts/LevelSelector/LSPlayer.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class LSPlayer : MonoBehaviour
 {
+    public MapPoint currentPoint;
+
+    public float moveSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,51 @@ public class LSPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.transform.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, currentPoint.transform.position) < 0.1f)
+        {
+            if(Input.GetAxisRaw("Horizontal") > 0.5f)
+            {
+                if(currentPoint.right != null)
+                {
+                    SetNextPoint(currentPoint.right);
+                }
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                if (currentPoint.left != null)
+                {
+                    SetNextPoint(currentPoint.left);
+                }
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0.5f)
+            {
+                if (currentPoint.up != null)
+                {
+                    SetNextPoint(currentPoint.up);
+                }
+            }
+
+            if (Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                if (currentPoint.down != null)
+                {
+                    SetNextPoint(currentPoint.down);
+                }
+            }
+            
+            if (currentPoint.isLevel && Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("Cambiamos el nivel");
+            }
+        }
+    }
+
+    public void SetNextPoint(MapPoint nextPoint)
+    {
+        currentPoint = nextPoint;
     }
 }
--- Assets/Code/Scripts/LevelSelector/MapPoint.cs
+++ Assets/Code/Scripts/LevelSelector/MapPoint.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class MapPoint : MonoBehaviour
 {
     public MapPoint up, right, down, left;
+    public bool isLevel;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
